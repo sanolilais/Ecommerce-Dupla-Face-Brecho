36868888a133be38fae23bc5f3c91689560f3715
Lais <sanolilais@gmail.com>
2022-03-29 19:49:16 -0300

    hamburguer menu, search bar andlogin icons - first steps

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function myfunction() {
+  let iconmenu = document.getElementsByClassName('linksmenu')
+  if (iconmenu.style.display === 'block') {
+    iconmenu.style.display = 'none'
+  } else {
+    iconmenu.style.display = 'block'
+  }
+}
